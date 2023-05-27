90ecd8a1667e2056445a31fe6a4d900dc6a46821
particlematter16 <anthony.poyet@upr.edu>
2023-05-27 03:54:07 -0400

    Read&Write RFID Upload

---
+++ Read&Write_RFID_chip.py
@@ -0,0 +1,18 @@
+import RPi.GPIO as GPIO
+from mfrc522 import SimpleMFRC522
+reader = SimpleMFRC522()
+try:
+        text = input('New data:')
+        print("Now place your tag to write")
+        reader.write(text)
+        print("Written")
+finally:
+    GPIO.cleanup()
+    
+while True:
+    try:
+        id, text = reader.read()
+        print(id)
+        print(text)
+    finally:
+        GPIO.cleanup()
